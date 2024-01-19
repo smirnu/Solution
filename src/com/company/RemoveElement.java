24bb6a38185ee8e1c88bb277897d75f8ccf6ab94
smirnu <77616227+smirnu@users.noreply.github.com>
2024-01-19 10:36:23 +0000

    Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.

---
+++ src/com/company/RemoveElement.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            if (nums[left] == val) {
+                nums[left] = nums[right];
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return left;
+    }
+}
